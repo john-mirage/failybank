ed8ca73465296d882b14e17a0cf62b0eeb0c5118
john-mirage <chaosdynamix@posteo.net>
2022-04-12 14:00:17 +0200

    feat: rework structure

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import "./assets/styles/global.css";
 import "./assets/styles/components/page.css";
 import "./assets/styles/components/dialog.css";
 import "./assets/styles/components/tab-list.css";
-import "./assets/styles/components/tab.css";
 import "./assets/styles/components/group.css";
 import "./assets/styles/components/text.css";
 import "./assets/styles/components/paper.css";
 
 import "./assets/styles/components/row.css";
 import "./assets/styles/components/column.css";
+import "./assets/styles/components/button.css";
+import "./assets/styles/components/input.css";
+import "./assets/styles/components/toggle-button.css";
 
 import "./assets/styles/states.css";
