67c77e68a3f93d35c0601d50993349d5e33a38ed
john-mirage <chaosdynamix@posteo.net>
2022-04-12 10:57:26 +0200

    feat: add basic account view

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import "./assets/styles/components/page.css";
 import "./assets/styles/components/dialog.css";
 import "./assets/styles/components/tab-list.css";
 import "./assets/styles/components/tab.css";
+import "./assets/styles/components/group.css";
+import "./assets/styles/components/text.css";
+import "./assets/styles/components/paper.css";
+
+import "./assets/styles/components/row.css";
+import "./assets/styles/components/column.css";
 
 import "./assets/styles/states.css";
