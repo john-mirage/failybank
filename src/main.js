4fd8e5c367f4da0288f903c57115010ccc705ab2
john-mirage <chaosdynamix@posteo.net>
2022-04-12 10:03:45 +0200

    feat: initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./assets/styles/global.css";
+
+import "./assets/styles/components/page.css";
+import "./assets/styles/components/dialog.css";
+import "./assets/styles/components/tab-list.css";
+import "./assets/styles/components/tab.css";
+
+import "./assets/styles/states.css";
