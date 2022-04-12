30a96bd748812410828fa1f26e7c3c910561f94c
john-mirage <chaosdynamix@posteo.net>
2022-04-12 20:14:52 +0200

    feat: add grid

--- src/main.js
+++ src/main.js
@@ -8,8 +8,11 @@ import "./assets/styles/components/paper.css";
 
 import "./assets/styles/components/row.css";
 import "./assets/styles/components/column.css";
+import "./assets/styles/components/grid.css";
 import "./assets/styles/components/button.css";
+import "./assets/styles/components/icon-button.css";
 import "./assets/styles/components/input.css";
 import "./assets/styles/components/toggle-button.css";
+import "./assets/styles/components/table.css";
 
 import "./assets/styles/states.css";
