67d4ecec6b78158f4c81f0e96cb0a8859ab90bc5
john-mirage <chaosdynamix@posteo.net>
2022-04-13 23:51:23 +0200

    feat: work on new layout

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import "./assets/styles/global.css";
 
-import "./assets/styles/components/page.css";
-import "./assets/styles/components/dialog.css";
+import "./assets/styles/components/screen.css";
+import "./assets/styles/components/app.css";
+import "./assets/styles/components/bar.css";
+import "./assets/styles/components/balance.css";
 import "./assets/styles/components/tab-list.css";
-import "./assets/styles/components/text.css";
-import "./assets/styles/components/paper.css";
-
-import "./assets/styles/components/row.css";
-import "./assets/styles/components/column.css";
-import "./assets/styles/components/grid.css";
-import "./assets/styles/components/button.css";
-import "./assets/styles/components/icon-button.css";
-import "./assets/styles/components/icon-box.css";
-import "./assets/styles/components/input.css";
 import "./assets/styles/components/toggle-button.css";
-import "./assets/styles/components/table.css";
+import "./assets/styles/components/view.css";
+import "./assets/styles/components/grid.css";
+import "./assets/styles/components/text.css";
+import "./assets/styles/components/column.css";
+import "./assets/styles/components/row.css";
+import "./assets/styles/components/paper.css";
+import "./assets/styles/components/input.css";
+import "./assets/styles/components/button.css";
 
 import "./assets/styles/states.css";
