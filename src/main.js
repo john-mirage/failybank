cb9fbc855cc528ef40ab31c62ab86ae54c243ca3
john-mirage <chaosdynamix@posteo.net>
2022-04-14 21:12:47 +0200

    fix: fix forms

--- src/main.js
+++ src/main.js
@@ -21,15 +21,72 @@ import "./assets/styles/components/icon-button.css";
 import "./assets/styles/states.css";
 
 const swiftAddForm = document.getElementById("swift-add-form");
+const depositForm = document.getElementById("deposit-form");
+const withdrawForm = document.getElementById("withdraw-form");
+const transferForm = document.getElementById("transfer-form");
+
+function validateAmount(amount) {
+    const search = amount.search(/^[0-9]+$/);
+    return search > -1;
+}
+
+function validateSwift(swift) {
+    const search = swift.search(/^[0-9]{10}$/);
+    return search > -1;
+}
 
 swiftAddForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(swiftAddForm);
-    const swiftCode = formData.get("swift-code");
-    const search = swiftCode.search("^[0-9]{10}$");
-    if (search > -1) {
+    const swift = formData.get("swift-code");
+    const swiftIsValid = validateSwift(swift);
+    if (swiftIsValid) {
         console.log("Swift code is valid");
+        swiftAddForm.reset();
     } else {
         console.log("Swift code is not valid");
+    }
+});
+
+depositForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(depositForm);
+    const depositValue = formData.get("deposit-amount");
+    const amountIsValid = validateAmount(depositValue);
+    if (amountIsValid) {
+        console.log("Deposit amount is valid");
+        depositForm.reset();
+    } else {
+        console.log("Deposit amount is not valid");
+    }
+});
+
+withdrawForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(withdrawForm);
+    const withdrawValue = formData.get("withdraw-amount");
+    const amountIsValid = validateAmount(withdrawValue);
+    if (amountIsValid) {
+        console.log("Withdraw amount is valid");
+        withdrawForm.reset();
+    } else {
+        console.log("Withdraw amount is not valid");
+    }
+});
+
+transferForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(transferForm);
+    const transferAmount = formData.get("transfer-amount");
+    const transferSwift = formData.get("transfer-swift");
+    const transferReference = formData.get("transfer-reference");
+    const amountIsValid = validateAmount(transferAmount);
+    const swiftIsValid = validateAmount(transferSwift);
+    const referenceIsValid = true;
+    if (amountIsValid && swiftIsValid && referenceIsValid) {
+        console.log("transfer values are valid");
+        transferForm.reset();
+    } else {
+        console.log("transfer values are not valid");
     }
 });
