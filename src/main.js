4eb965f63af9bba5eb68a52a93bf783aecb2c110
john-mirage <chaosdynamix@posteo.net>
2022-04-15 11:35:30 +0200

    feat: rework account view

--- src/main.js
+++ src/main.js
@@ -10,13 +10,12 @@ import "./assets/styles/components/view.css";
 import "./assets/styles/components/grid.css";
 import "./assets/styles/components/text.css";
 import "./assets/styles/components/column.css";
-import "./assets/styles/components/row.css";
 import "./assets/styles/components/paper.css";
-import "./assets/styles/components/input.css";
-import "./assets/styles/components/button.css";
 import "./assets/styles/components/divider.css";
 import "./assets/styles/components/table.css";
 import "./assets/styles/components/icon-button.css";
+import "./assets/styles/components/form.css";
+import "./assets/styles/components/section.css";
 
 import "./assets/styles/states.css";
 
