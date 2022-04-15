fdf577dd2acc424fb6250f0a0106125a6207ddf0
john-mirage <chaosdynamix@posteo.net>
2022-04-15 23:49:12 +0200

    feat: add account, operation and transfer form validation

--- src/main.js
+++ src/main.js
@@ -18,14 +18,32 @@ import "./assets/styles/components/log.css";
 
 import "./assets/styles/states.css";
 
-/**
- * Transfer past System for the swift codes.
- */
+const swiftAddForm = document.getElementById("swift-add-form");
+const swiftAddInput = document.getElementById("swift-add-input");
+const swiftAddMessage = document.getElementById("swift-add-message");
+
+const depositForm = document.getElementById("deposit-form");
+const depositInput = document.getElementById("deposit-input");
+const depositMessage = document.getElementById("deposit-message");
+
+const withdrawForm = document.getElementById("withdraw-form");
+const withdrawInput = document.getElementById("withdraw-input");
+const withdrawMessage = document.getElementById("withdraw-message");
+
 const transferSwiftRows = document.getElementsByClassName("swift--transfer");
 const transferSwiftTexts = document.getElementsByClassName("swift__code--transfer");
 const transferSwiftButtons = document.getElementsByClassName("swift__icon-button--transfer");
 const transferSwiftInput = document.getElementById("transfer-swift-input");
+const transferSwiftMessage = document.getElementById("transfer-swift-message");
+const transferForm = document.getElementById("transfer-form");
+const transferAmountInput = document.getElementById("transfer-amount-input");
+const transferReferenceInput = document.getElementById("transfer-reference-input");
+const transferAmountMessage = document.getElementById("transfer-amount-message");
+const transferReferenceMessage = document.getElementById("transfer-reference-message");
 
+/**
+ * Transfer past System for the swift codes.
+ */
 for (let rowIndex = 0; rowIndex < transferSwiftRows.length; rowIndex++) {
     transferSwiftButtons[rowIndex].addEventListener("click", () => {
         if (transferSwiftInput.value !== transferSwiftTexts[rowIndex].textContent) {
@@ -37,23 +55,97 @@ for (let rowIndex = 0; rowIndex < transferSwiftRows.length; rowIndex++) {
 /**
  * Form validations
  */
-const swiftAddForm = document.getElementById("swift-add-form");
-const depositForm = document.getElementById("deposit-form");
-const withdrawForm = document.getElementById("withdraw-form");
-const transferForm = document.getElementById("transfer-form");
-
 swiftAddForm.addEventListener("submit", (event) => {
+    if (!swiftAddInput.validity.valid) {
+        if (swiftAddInput.validity.valueMissing) {
+            swiftAddMessage.textContent = "Veuillez entrer le code SWIFT";
+        } else if (swiftAddInput.validity.tooShort || swiftAddInput.validity.tooLong) {
+            swiftAddMessage.textContent = "Le code SWIFT doit comporter exactement 10 chiffres";
+        } else if (swiftAddInput.validity.patternMismatch) {
+            swiftAddMessage.textContent = "Le code SWIFT ne doit comporter que des chiffres";
+        }
+    } else {
+        swiftAddForm.reset();
+        if (swiftAddMessage.textContent.length > 0) {
+            swiftAddMessage.textContent = "";
+        }
+    }
     event.preventDefault();
 });
 
 depositForm.addEventListener("submit", (event) => {
+    if (!depositInput.validity.valid) {
+        if (depositInput.validity.valueMissing) {
+            depositMessage.textContent = "Veuillez entrer le montant";
+        } else if (depositInput.validity.patternMismatch) {
+            depositMessage.textContent = "Le montant ne doit comporter que des chiffres";
+        }
+    } else {
+        depositForm.reset();
+        if (depositMessage.textContent.length > 0) {
+            depositMessage.textContent = "";
+        }
+    }
     event.preventDefault();
 });
 
 withdrawForm.addEventListener("submit", (event) => {
+    if (!withdrawInput.validity.valid) {
+        if (withdrawInput.validity.valueMissing) {
+            withdrawMessage.textContent = "Veuillez entrer le montant";
+        } else if (withdrawInput.validity.patternMismatch) {
+            withdrawMessage.textContent = "Le montant ne doit comporter que des chiffres";
+        }
+    } else {
+        withdrawForm.reset();
+        if (withdrawMessage.textContent.length > 0) {
+            withdrawMessage.textContent = "";
+        }
+    }
     event.preventDefault();
 });
 
 transferForm.addEventListener("submit", (event) => {
+    if (!transferAmountInput.validity.valid) {
+        if (transferAmountInput.validity.valueMissing) {
+            transferAmountMessage.textContent = "Veuillez entrer un montant";
+        } else if (transferAmountInput.validity.patternMismatch) {
+            transferAmountMessage.textContent = "Le montant ne doit comporter que des chiffres";
+        }
+    } else if (transferAmountInput.validity.valid) {
+        if (transferAmountMessage.textContent.length > 0) {
+            transferAmountMessage.textContent = "";
+        }
+    }
+
+    if (!transferSwiftInput.validity.valid) {
+        if (transferSwiftInput.validity.valueMissing) {
+            transferSwiftMessage.textContent = "Veuillez entrer le code SWIFT";
+        } else if (transferSwiftInput.validity.tooShort || transferSwiftInput.validity.tooLong) {
+            transferSwiftMessage.textContent = "Le code SWIFT doit comporter exactement 10 chiffres";
+        } else if (transferSwiftInput.validity.patternMismatch) {
+            transferSwiftMessage.textContent = "Le code SWIFT ne doit comporter que des chiffres";
+        }
+    } else if (transferSwiftInput.validity.valid) {
+        if (transferSwiftMessage.textContent.length > 0) {
+            transferSwiftMessage.textContent = "";
+        }
+    }
+
+    if (!transferReferenceInput.validity.valid) {
+        if (transferReferenceInput.validity.valueMissing) {
+            transferReferenceMessage.textContent = "Veuillez entrer la référence du transfert";
+        } else if (transferReferenceInput.validity.tooLong) {
+            transferReferenceMessage.textContent = "La référence du transfert ne doit exeder 40 caractères";
+        }
+    } else if (transferReferenceInput.validity.valid) {
+        if (transferReferenceMessage.textContent.length > 0) {
+            transferReferenceMessage.textContent = "";
+        }
+    }
+
+    if (transferAmountInput.validity.valid && transferSwiftInput.validity.valid && transferReferenceInput.validity.valid) {
+        transferForm.reset();
+    }
     event.preventDefault();
 });
