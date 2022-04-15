3823b83801db203a2372db4ad1fac4a18ff47eba
john-mirage <chaosdynamix@posteo.net>
2022-04-15 11:52:43 +0200

    feat: rework operation view

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ import "./assets/styles/components/grid.css";
 import "./assets/styles/components/text.css";
 import "./assets/styles/components/column.css";
 import "./assets/styles/components/paper.css";
-import "./assets/styles/components/divider.css";
 import "./assets/styles/components/table.css";
 import "./assets/styles/components/icon-button.css";
 import "./assets/styles/components/form.css";
