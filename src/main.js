b67241c926c94fc28335d6fe7adab77108b8a511
john-mirage <chaosdynamix@posteo.net>
2022-05-10 04:08:42 +0200

    feat: work on account and fix enterprise and offshore tabs

--- src/main.js
+++ src/main.js
@@ -26,17 +26,6 @@ const personalFavoriteAccountNameMessage = document.getElementById("personal-fav
 const personalFavoriteAccountNumberInput = document.getElementById("personal-favorite-account-number-input");
 const personalFavoriteAccountNumberMessage = document.getElementById("personal-favorite-account-number-message");
 
-const enterpriseDepositForm = document.getElementById("enterprise-deposit-form");
-const enterpriseWithdrawForm = document.getElementById("enterprise-withdraw-form");
-const enterpriseDepositAmountInput = document.getElementById("enterprise-deposit-amount-input");
-const enterpriseDepositAmountMessage = document.getElementById("enterprise-deposit-amount-message");
-const enterpriseWithdrawAmountInput = document.getElementById("enterprise-withdraw-amount-input");
-const enterpriseWithdrawAmountMessage = document.getElementById("enterprise-withdraw-amount-message");
-
-const offshoreDepositForm = document.getElementById("offshore-deposit-form");
-const offshoreDepositAmountInput = document.getElementById("offshore-deposit-amount-input");
-const offshoreDepositAmountMessage = document.getElementById("offshore-deposit-amount-message");
-
 /*------------------------------------*\
   Formatters
 \*------------------------------------*/
@@ -151,10 +140,100 @@ function checkReferenceField(input, message) {
 }
 
 /*------------------------------------*\
+  Account classes
+\*------------------------------------*/
+
+class Account {
+  constructor(
+    account,
+    ownerElement,
+    balanceElement,
+    logListElement
+  ) {
+    this.owner = account.owner;
+    this.ownerElement = ownerElement;
+    this.balance = account.balance;
+    this.balanceElement = balanceElement;
+    //this.logList = new LogList(account.logs, logListElement);
+    this.displayOwner();
+    this.displayBalance();
+  }
+
+  displayOwner() {
+    this.ownerElement.textContent = this.owner;
+  }
+
+  displayBalance() {
+    if (this.balance < 1) {
+      if (this.balanceElement.classList.contains("section__number--up")) {
+        this.balanceElement.classList.replace("section__number--up","section__number--down");
+      } else if (!this.balanceElement.classList.contains("section__number--down")) {
+        this.balanceElement.classList.add("section__number--down");
+      }
+    } else {
+      if (this.balanceElement.classList.contains("section__number--down")) {
+        this.balanceElement.classList.replace("section__number--down","section__number--up");
+      } else if (!this.balanceElement.classList.contains("section__number--up")) {
+        this.balanceElement.classList.add("section__number--up");
+      }
+    }
+    this.balanceElement.textContent = currencyFormatter.format(this.balance);
+  }
+}
+
+class PersonalAccount extends Account {
+  constructor(
+    account,
+    ownerElement,
+    balanceElement,
+    logListElement,
+    numberElement,
+    operationLogListElement
+  ) {
+    super(
+      account,
+      ownerElement,
+      balanceElement,
+      logListElement,
+    );
+    this.number = account.number;
+    this.numberElement = numberElement;
+    this.theme = account.theme;
+    //this.accountList = new AccountList(account.favoriteAccounts);
+    //this.operationLogList = new LogList(account.logs.filter(log => log.type === "operation"), operationLogListElement);
+    this.displayNumber();
+  }
+
+  displayNumber() {
+    this.numberElement.textContent = this.number;
+  }
+
+  displayBalance() {
+    if (this.balance < 1) {
+      if (!this.balanceElement.classList.contains("balance__value--down")) {
+        this.balanceElement.classList.add("balance__value--down");
+      }
+    } else {
+      if (this.balanceElement.classList.contains("balance__value--down")) {
+        this.balanceElement.classList.remove("balance__value--down");
+      }
+    }
+    this.balanceElement.textContent = currencyFormatter.format(this.balance);
+  }
+}
+
+/*------------------------------------*\
   Personal account
 \*------------------------------------*/
 
-const personalAccount = new PersonalAccount(data.account.personal);
+const personalAccount = new PersonalAccount(
+  data.account.personal,
+  document.getElementById("personal-account-owner"),
+  document.getElementById("personal-account-balance"),
+  document.getElementById("personal-account-log-list"),
+  document.getElementById("personal-account-number"),
+  document.getElementById("personal-account-operation-log-list")
+);
 
 function handlePersonalThemeButton(event) {
   personalAccount.theme = event.target.checked ? "dark" : "light";
@@ -216,11 +295,23 @@ personalWithdrawForm.addEventListener("submit", handlePersonalWithdrawForm);
 personalTransferForm.addEventListener("submit", handlePersonalTransferForm);
 
 /*------------------------------------*\
-  Enterprise account
+  Enterprise & Offshore accounts
 \*------------------------------------*/
 
 if (data.hasEnterprise) {
-  const enterpriseAccount = new EnterpriseAccount(data.account.enterprise);
+  const enterpriseDepositForm = document.getElementById("enterprise-deposit-form");
+  const enterpriseWithdrawForm = document.getElementById("enterprise-withdraw-form");
+  const enterpriseDepositAmountInput = document.getElementById("enterprise-deposit-amount-input");
+  const enterpriseDepositAmountMessage = document.getElementById("enterprise-deposit-amount-message");
+  const enterpriseWithdrawAmountInput = document.getElementById("enterprise-withdraw-amount-input");
+  const enterpriseWithdrawAmountMessage = document.getElementById("enterprise-withdraw-amount-message");
+
+  const enterpriseAccount = new Account(
+    data.account.enterprise,
+    document.getElementById("enterprise-account-owner"),
+    document.getElementById("enterprise-account-balance"),
+    document.getElementById("enterprise-account-log-list")
+  );
 
   function handleEnterpriseDepositForm(event) {
     event.preventDefault();
@@ -241,24 +332,39 @@ if (data.hasEnterprise) {
 
   enterpriseDepositForm.addEventListener("submit", handleEnterpriseDepositForm);
   enterpriseWithdrawForm.addEventListener("submit", handleEnterpriseWithdrawForm);
-}
 
-/*------------------------------------*\
-  Offshore account
-\*------------------------------------*/
+  if (data.hasOffshore) {
+    const offshoreDepositForm = document.getElementById("offshore-deposit-form");
+    const offshoreDepositAmountInput = document.getElementById("offshore-deposit-amount-input");
+    const offshoreDepositAmountMessage = document.getElementById("offshore-deposit-amount-message");
 
-if (data.hasOffshore) {
-  const offshoreAccount = new OffshoreAccount(data.account.offshore);
+    const offshoreAccount = new Account(
+      data.account.offshore,
+      document.getElementById("offshore-account-owner"),
+      document.getElementById("offshore-account-balance"),
+      document.getElementById("offshore-account-log-list")
+    );
 
-  function handleOffshoreDepositForm(event) {
-    event.preventDefault();
-    const amountFieldIsValid = checkAmountField(offshoreDepositAmountInput, offshoreDepositAmountMessage);
-    if (amountFieldIsValid) {
-      const depositAmount = Number(offshoreDepositAmountInput.value);
+    function handleOffshoreDepositForm(event) {
+      event.preventDefault();
+      const amountFieldIsValid = checkAmountField(offshoreDepositAmountInput, offshoreDepositAmountMessage);
+      if (amountFieldIsValid) {
+        const depositAmount = Number(offshoreDepositAmountInput.value);
+      }
     }
-  }
 
-  offshoreDepositForm.addEventListener("submit", handleOffshoreDepositForm);
+    offshoreDepositForm.addEventListener("submit", handleOffshoreDepositForm);
+  } else {
+    const offshoreTab = document.getElementById("tab-offshore");
+    const offshoreTabContent = document.getElementById("tab-content-offshore");
+    offshoreTab.remove();
+    offshoreTabContent.remove();
+  }
+} else {
+  const enterpriseTab = document.getElementById("tab-enterprise");
+  const enterpriseTabContent = document.getElementById("tab-content-enterprise");
+  enterpriseTab.remove();
+  enterpriseTabContent.remove();
 }
 
 /*------------------------------------*\
