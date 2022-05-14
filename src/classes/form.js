10820e1659fbd8f16e9abcedbbff62a931d0f1a4
john-mirage <chaosdynamix@posteo.net>
2022-05-14 04:49:52 +0200

    feat: add form input debounce method

--- src/classes/form.js
+++ src/classes/form.js
@@ -4,12 +4,22 @@ export class Form {
     this.formElement = formElement;
     this.buttonElement = buttonElement;
     this.buttonIsActive = false;
+    this.CheckFieldsWithDebounce = this.debounce(() => this.checkFields());
     this.inputElements.forEach((inputElement) => {
-      inputElement.addEventListener("keyup", () => this.checkFields());
+      inputElement.addEventListener("keyup", () => this.CheckFieldsWithDebounce());
     });
   }
 
+  debounce(callback, timeout = 300) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { callback.apply(this, args); }, timeout);
+    };
+  }
+
   checkFields() {
+    console.log("check fields")
     const fieldsAreValid = this.inputElements.every((field) => field.validity.valid);
     if (fieldsAreValid && !this.buttonIsActive) {
       this.activateSubmitButton();
