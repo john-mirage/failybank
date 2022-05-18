adfd0e1ad51a2ee5262cef1946076b912bf51833
john-mirage <chaosdynamix@posteo.net>
2022-05-18 18:59:07 +0200

    ref: rework icons

--- src/scripts/classes/notification-list.js
+++ src/scripts/classes/notification-list.js
@@ -5,11 +5,23 @@ export class NotificationList {
     this.notificationListElement = notificationListElement;
   }
 
+  getNotificationIcon(type) {
+    switch (type) {
+      case "success":
+        return  "#icon-check";
+      case "error":
+        return  "#icon-error";
+    }
+  }
+
   createNotification(notification) {
     const notificationFragment = notificationTemplate.content.cloneNode(true);
     const notificationElement = notificationFragment.querySelector(".notification");
-    const notificationTitle = notificationFragment.querySelector('[data-name="notification-name"]');
-    const notificationDescription = notificationFragment.querySelector('[data-name="notification-description"]');
+    const notificationTitle = notificationElement.querySelector('[data-name="notification-name"]');
+    const notificationDescription = notificationElement.querySelector('[data-name="notification-description"]');
+    const notificationIconUse = notificationElement.querySelector('[data-name="notification-icon"]');
+    const iconName = this.getNotificationIcon(notification.type);
+    notificationIconUse.setAttribute("href", iconName);
     notificationElement.classList.add(`notification--${notification.type}`);
     notificationTitle.textContent = notification.title;
     notificationDescription.textContent = notification.description;
